b381e3ac1323f30761b3409d396e7a4df34c7fe1
Sami Al-Dury <git@aldury.com>
2023-10-17 23:58:14 +0200

    fix: bundle configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,5 +1,5 @@
+import { spawn } from 'node:child_process'
 import { defineConfig } from 'tsup'
-import { spawn } from 'child_process'
 
 export default defineConfig({
     clean: true,
@@ -8,7 +8,13 @@ export default defineConfig({
     bundle: true,
     target: 'es2022',
     outDir: 'dist',
+    dts: true,
+    sourcemap: true,
     onSuccess: async () => {
+        // Sleep for 3 sec, workaround for:
+        // https://github.com/egoist/tsup/issues/700
+        await new Promise((resolve) => setTimeout(resolve, 3000))
+
         return new Promise((resolve) => {
             console.log('\nBuild succeeded!\n')
             console.log('Generating declaration files...\n')
